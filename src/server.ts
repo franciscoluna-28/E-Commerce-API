9a62b388a893fc4f1fc08ceedc94a04368a85953
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-22 19:56:22 -0400

    Server initialization

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import productRoutes from "./application/routes/Product";
+
+const app = express();
+
+app.use("/products", productRoutes);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
